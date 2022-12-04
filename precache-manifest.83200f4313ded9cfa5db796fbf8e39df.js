b67c03a8b79f65e6f968d19651f5a7b36add87f2
Jon (as Circle Bot) <berkona@gmail.com>
2022-12-04 13:59:51 +0000

    Deploy to GitHub pages [ci skip]

---
+++ precache-manifest.83200f4313ded9cfa5db796fbf8e39df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6e03c4b31e135f667d4a78b1dbfdc2",
+    "url": "/project-fenris/index.html"
+  },
+  {
+    "revision": "eb7cd7595360571772cf",
+    "url": "/project-fenris/static/js/2.305d2d05.chunk.js"
+  },
+  {
+    "revision": "f9d70cd082e88d501038",
+    "url": "/project-fenris/static/js/main.fd39c21f.chunk.js"
+  },
+  {
+    "revision": "3380d2e98065b63c1316",
+    "url": "/project-fenris/static/js/runtime-main.d6053c7c.js"
+  }
+]);
